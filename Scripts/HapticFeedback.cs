0b5386491a00e71608d2760a47c41e61a5fdfc91
Abhi86shek <sonkar.abhishek86@gmail.com>
2024-02-08 13:46:01 +0530

    Update HapticFeedback.cs

--- Scripts/HapticFeedback.cs
+++ Scripts/HapticFeedback.cs
@@ -2,7 +2,7 @@ namespace SAS.HapticFeedback
 {
     public static class HapticFeedback
     {
-        public static bool IsHapticFeedbackEnabled { get; set; } = true;
+        public static bool IsHapticFeedbackEnabled { get; set; }
         public static void LightFeedback()
         {
             if (!IsHapticFeedbackEnabled) return;
